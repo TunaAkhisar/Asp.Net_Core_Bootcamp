00282cbfd5d6718d7dc0e81acc8381069a5011f1
Tunahan Akhisar <tunaakhisar9@gmail.com>
2024-01-27 17:07:01 +0300

    EF Core & Database Operations

---
+++ efCore/Data/Kurs.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+
+namespace efCore.Data
+{
+    public class Kurs
+    {
+        [Key]
+        public int KursId { get; set; }
+        public string? Baslik { get; set; }
+    }
+}
